65ec3e52734e3ba0a10ed7aa8b7a8b08935ab61b
Viktor Breza <brezaviktor@gmail.com>
2022-05-05 22:49:02 +0300

    first day completed

---
+++ day_1.py
@@ -0,0 +1,13 @@
+#print(len(input('What is your name? '))) #showing length of string
+
+'''
+a = int(input('a = '))  #changing value between two variables
+b = int(input('b = '))
+
+c = a
+a = b
+b = c
+
+print('a = ' + str(a))
+print('b = ' + str(b))
+'''
